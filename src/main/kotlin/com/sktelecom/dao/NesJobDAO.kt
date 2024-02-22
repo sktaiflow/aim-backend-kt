4f72a95b1fc172f7cff83f9bb4ca579b07da1e8b
유상기 <lapetus@sktai.io>
2024-02-22 20:06:43 +0900

    리팩토링

--- src/main/kotlin/com/sktelecom/dao/NesJobDAO.kt
+++ src/main/kotlin/com/sktelecom/dao/NesJobDAO.kt
@@ -1,7 +1,40 @@
 package com.sktelecom.dao
 
 import com.sktelecom.models.NesJob
+import com.sktelecom.models.NesJobs
+import kotlinx.coroutines.runBlocking
+import org.jetbrains.exposed.sql.ResultRow
+import org.jetbrains.exposed.sql.select
 
 interface NesJobDAO {
     suspend fun nesJobs(): List<NesJob>
 }
+
+class NesJobDAOImpl : NesJobDAO {
+    private fun resultRowToNesJob(row: ResultRow) =
+        NesJob(
+            id = row[NesJobs.id],
+            userId = row[NesJobs.userId],
+            jobId = row[NesJobs.jobId],
+            notebookUrl = row[NesJobs.notebookUrl],
+            outputUrl = row[NesJobs.outputUrl],
+            runtimeName = row[NesJobs.runtimeName],
+            runtimeSpec = row[NesJobs.runtimeSpec],
+            runtimeImage = row[NesJobs.runtimeImage],
+            runtimeTag = row[NesJobs.runtimeTag],
+            parameters = row[NesJobs.parameters],
+            lastState = row[NesJobs.lastState]
+//            createdAt = row[NesJobs.createdAt]
+        )
+
+    override suspend fun nesJobs(): List<NesJob> =
+        Database.dbQuery {
+            NesJobs.select { NesJobs.userId eq "1112369" }.map(::resultRowToNesJob)
+        }
+}
+
+val nesJobDao: NesJobDAO =
+    NesJobDAOImpl().apply {
+        runBlocking {
+        }
+    }
--- src/main/kotlin/com/sktelecom/dao/UserDAO.kt
+++ src/main/kotlin/com/sktelecom/dao/UserDAO.kt
@@ -1,7 +1,30 @@
 package com.sktelecom.dao
 
 import com.sktelecom.models.User
+import com.sktelecom.models.Users
+import kotlinx.coroutines.runBlocking
+import org.jetbrains.exposed.sql.ResultRow
+import org.jetbrains.exposed.sql.selectAll
 
 interface UserDAO {
     suspend fun users(): List<User>
 }
+
+class UserDAOImpl : UserDAO {
+    private fun resultRowToUser(row: ResultRow) =
+        User(
+            id = row[Users.id],
+            name = row[Users.name]
+        )
+
+    override suspend fun users(): List<User> =
+        Database.dbQuery {
+            Users.selectAll().map(::resultRowToUser)
+        }
+}
+
+val userDao: UserDAO =
+    UserDAOImpl().apply {
+        runBlocking {
+        }
+    }
