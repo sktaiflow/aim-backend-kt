ec019becb7be990f8574bb7efa1fda29283c09a5
유상기 <lapetus@sktai.io>
2024-02-22 15:41:32 +0900

    스케줄러 테스트

--- src/main/kotlin/com/sktelecom/Application.kt
+++ src/main/kotlin/com/sktelecom/Application.kt
@@ -5,8 +5,12 @@ import com.sktelecom.plugins.configureRouting
 import io.ktor.server.application.Application
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import java.util.*
+import kotlin.concurrent.scheduleAtFixedRate
 
 fun main() {
+    Timer().scheduleAtFixedRate(0, 60 * 1000) {
+    }
     embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
         .start(wait = true)
 }
